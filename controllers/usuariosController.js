4fda477de39a652093081834b7651c163dcb75ba
Juan Genovard <juangenovardcaballero@gmail.com>
2023-01-15 18:13:27 +0100

    provando

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -3,31 +3,6 @@ const { encryptContraseña } = require('../services/authServices');
 
 const usuariosController = {};
 
-// usuariosController.getAllUsuarios = async (req, res) => {
-//     try {
-//         let resp = await usuarios.findAll({attributes: {exclude:['createdAt', 'updatedAt']},
-//         })
-//             .then(resp => {
-//                 res.send(resp)
-//             })
-//     } catch (err) {
-//         res.send(err)
-//     }
-// }
-
-// usuariosController.getUsuariosByEmail = async (req, res) => {
-//     try {
-//         let email = req.params.email
-//         let resp = await usuarios.findOne({ attributes: {exclude:['createdAt', 'updatedAt']},
-//             where: {email: email}
-//         })
-//             .then(resp => {
-//                 res.send(resp)
-//             })
-//     } catch (err) {
-//         res.send(err)
-//     }
-// }
 usuariosController.postNuevoUsuario = async (req, res) => {
     try {
         let data = req.body
